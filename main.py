10bf8e831ccaf3d9090655a84bb08433c04bf609
Tibabalase Oludemi <30732454+lafftar@users.noreply.github.com>
2021-07-10 18:15:37 -0400

    maint

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import sys
+
+from newegg.utils.return_project_root import get_project_root
+
 try:
     import os
     import platform
@@ -9,6 +12,7 @@ try:
     import colorama
     from colorama import Fore
     from colorama import init
+
     colorama.init()
     init(autoreset=True)
     from captchatools import captcha_harvesters
@@ -20,7 +24,6 @@ except ModuleNotFoundError:
     sys.exit(0)
 
 
-
 class Newegg:
     def __init__(self):
         self.screenlock = Semaphore(value=1)
@@ -28,30 +31,32 @@ class Newegg:
         # Load config
         settings = self.load_settings()
         self.settings = settings
-        if settings["captcha"]["captcha_solver"] == 1 or str( settings["captcha"]["captcha_solver"]).lower() == "capmonster":
+        if settings["captcha"]["captcha_solver"] == 1 or str(
+                settings["captcha"]["captcha_solver"]).lower() == "capmonster":
             api_key = settings["captcha"]["keys"]["capmonster"]
-        elif settings["captcha"]["captcha_solver"] == 2 or str( settings["captcha"]["captcha_solver"]).lower() == "anticaptcha":
+        elif settings["captcha"]["captcha_solver"] == 2 or str(
+                settings["captcha"]["captcha_solver"]).lower() == "anticaptcha":
             api_key = settings["captcha"]["keys"]["anticaptcha"]
-        elif settings["captcha"]["captcha_solver"] == 3 or str( settings["captcha"]["captcha_solver"]).lower() == "2captcha":
+        elif settings["captcha"]["captcha_solver"] == 3 or str(
+                settings["captcha"]["captcha_solver"]).lower() == "2captcha":
             api_key = settings["captcha"]["keys"]["2captcha"]
-        
+
         if settings["captcha"]["version"].lower() == "v3":
             sitekey = static_data.v3_sitekey
         elif settings["captcha"]["version"].lower() == "v2":
             sitekey = static_data.v2_sitekey
-        
 
         self.captcha_solver = captcha_harvesters(
-                                solving_site=settings["captcha"]["captcha_solver"], 
-                                api_key=api_key, 
-                                sitekey=sitekey,
-                                captcha_url="https://secure.newegg.com/identity/signup?tk=",
-                                min_score=0.7, action="Register", captcha_type=settings["captcha"]["version"]
+            solving_site=settings["captcha"]["captcha_solver"],
+            api_key=api_key,
+            sitekey=sitekey,
+            captcha_url="https://secure.newegg.com/identity/signup?tk=",
+            min_score=0.7, action="Register", captcha_type=settings["captcha"]["version"]
         )
 
         # Start the bot when initialized.
         self.main()
-    
+
     def main(self):
         '''
         Main method.
@@ -62,35 +67,31 @@ class Newegg:
 
         # Get user input for what they want to do
         print()
-        print(Fore.CYAN + '1' ,Fore.MAGENTA + "=", Fore.YELLOW +  "Create Accounts")
-        print(Fore.CYAN + '2' ,Fore.MAGENTA + "=", Fore.YELLOW +  "Submit Entries")
+        print(Fore.CYAN + '1', Fore.MAGENTA + "=", Fore.YELLOW + "Create Accounts")
+        print(Fore.CYAN + '2', Fore.MAGENTA + "=", Fore.YELLOW + "Submit Entries")
         choice = eval(input("{}".format(Fore.MAGENTA + "Enter your choice: " + Fore.CYAN)))
         if choice == 1:
             self.create_accounts()
         elif choice == 2:
             self.submit_entries()
-    
+
     def load_settings(self):
         # Some users said their program couldn't find the files
         # So this is a quick fix to that.
-        original_path = os.getcwd()
-        os.chdir("./data/")
-        self.PATH_EMAILS  = os.getcwd() + "\\emails.txt"
-        self.PATH_PROXIES  = os.getcwd() + "\\proxies.txt"
+        self.PATH_EMAILS = f'{get_project_root()}/data/emails.txt'
+        self.PATH_PROXIES = f'{get_project_root()}/data/proxies.txt'
         if platform.system() == "Linux" or platform.system() == "Darwin":
-            self.PATH_EMAILS  = self.PATH_EMAILS.replace("\\", "/")
-            self.PATH_PROXIES  = self.PATH_PROXIES.replace("\\", "/")
-        
+            self.PATH_EMAILS = self.PATH_EMAILS.replace("\\", "/")
+            self.PATH_PROXIES = self.PATH_PROXIES.replace("\\", "/")
 
         # Open settings file
-        settings_file_path = os.getcwd() + "/settings.json"
-        os.chdir(original_path)
+        settings_file_path = f'{get_project_root()}/data/settings.json'
         with open(settings_file_path) as settingsFile:
             try:
                 return json.load(settingsFile)
             except json.JSONDecodeError:
                 print(Fore.RED + "There is an error in your settings.json file." +
-                    "Make sure the commas are correct and there aren't any extra commas!")
+                      "Make sure the commas are correct and there aren't any extra commas!")
                 time.sleep(999)
                 sys.exit(0)
 
@@ -114,10 +115,10 @@ class Newegg:
             for line in email_file.readlines():
                 email = line.split(':')[0].strip("\n")
                 self.sema.acquire()
-                thread = threading.Thread(target=desktop.Create_Account,args=(self,email))
+                thread = threading.Thread(target=desktop.Create_Account, args=(self, email))
                 threads.append(thread)
                 thread.start()
-    
+
     def submit_entries(self):
         '''
         This method will be called when the user wants to enter the Newegg raffle.
@@ -127,7 +128,7 @@ class Newegg:
         '''
         from newegg.submit_raffle import raffles, app, desktop
         path_to_created_accounts = os.getcwd() + "\\{}".format(self.settings["output_filename"]) if \
-            platform.system == "Windows"  else os.getcwd() + "/{}".format(self.settings["output_filename"])
+            platform.system == "Windows" else os.getcwd() + "/{}".format(self.settings["output_filename"])
 
         # Because Newegg still has V2 captcha enabled for submitting entries,
         # we'll use that over V3. If for whatever reason you want to use V3,
@@ -141,42 +142,40 @@ class Newegg:
         items = raffles.get_items()
         index = 0
         for item in items:
-            print(Fore.GREEN + str(index) ,Fore.MAGENTA + "=", Fore.CYAN + "{} (${})".format( item["name"], item["price"]))
-            index +=1
-
+            print(Fore.GREEN + str(index), Fore.MAGENTA + "=",
+                  Fore.CYAN + "{} (${})".format(item["name"], item["price"]))
+            index += 1
 
         # Get user choice(s) for the items they want to enter for
-        self.PRODUCTS_TO_ENTER = [] # This will be the array used to submit entries
-        choices = input(Fore.MAGENTA + "Enter the numbers of all the products you'd like to enter for, seperated by a space. (Example: 1 5 7 9 ): " + Fore.GREEN)
+        self.PRODUCTS_TO_ENTER = []  # This will be the array used to submit entries
+        choices = input(
+            Fore.MAGENTA + "Enter the numbers of all the products you'd like to enter for, seperated by a space. (Example: 1 5 7 9 ): " + Fore.GREEN)
         for choice in choices.split():
             try:
                 self.PRODUCTS_TO_ENTER.append({"ItemNumber": items[eval(choice)]["id"]})
             except Exception:
                 print(Fore.RED + f"There was an error with item: #{choice}. Skipping that item.")
 
-
         # Show how many accounts have been made / are about to enter the raffle
         print()
-        try:    
+        try:
             with open(path_to_created_accounts) as email_file:
-                print( Fore.BLUE + "Amount of accounts loaded: {} ({})".format(
-                    Fore.CYAN + str(len(email_file.readlines())), 
+                print(Fore.BLUE + "Amount of accounts loaded: {} ({})".format(
+                    Fore.CYAN + str(len(email_file.readlines())),
                     self.settings["output_filename"])
-                )
+                      )
         except (FileExistsError, FileNotFoundError):
             print(Fore.RED + "Could not find the file with sessions. Check repo fore help.")
             time.sleep(100)
             return
-
 
         # Prompt how many tasks
         num_tasks = eval(input(Fore.BLUE + "Enter amount of tasks at a time: " + Fore.WHITE + " "))
         self.sema = threading.Semaphore(value=num_tasks)
         threads = []
 
-
         # Enter raffle
-        with open(path_to_created_accounts) as email_file: # Get the emails from the file the user named
+        with open(path_to_created_accounts) as email_file:  # Get the emails from the file the user named
             for line in email_file.readlines():
                 if "error" not in line:
                     self.sema.acquire()
@@ -185,10 +184,10 @@ class Newegg:
 
                     # If the acct was created via desktop, it will enter via that way
                     # If the acct was created via app, it will enter via that way
-                    if sessionData["desktop"] == {}: # If there are no desktop cookies, means it was created via APP
-                        task = threading.Thread(target=app.SubmitRaffle,args=(self,email, sessionData))
+                    if sessionData["desktop"] == {}:  # If there are no desktop cookies, means it was created via APP
+                        task = threading.Thread(target=app.SubmitRaffle, args=(self, email, sessionData))
                     else:
-                        task = threading.Thread(target=desktop.SubmitRaffle,args=(self,email, sessionData))
+                        task = threading.Thread(target=desktop.SubmitRaffle, args=(self, email, sessionData))
                     threads.append(task)
                     task.start()
 
